a82b0354b8897e8e9db0ca71895f29ee2c6eb5cb
App Academy Student <student@appacademy.io>
2017-03-30 14:28:54 -0700

    Made less super evil

--- board.rb
+++ board.rb
@@ -11,10 +11,11 @@ class Board
   end
 
   def self.from_file(filename)
-    rows = File.readlines("filename").map(:chomp)
+    rows = File.readlines(filename).map(&:chomp)
+    rows.reject! { |row| row == "" }
     tiles = rows.map do |row|
-      nums = row.split("").map { |char| parseInt(char) }
-      nums.map { |num| Tle.new(num) }
+      nums = row.split("").map { |char| Integer(char) }
+      nums.map { |num| Tile.new(num) }
     end
 
     self.new(tiles)
@@ -25,22 +26,21 @@ class Board
   end
 
   def [](pos)
-    pos = x,y
+    x, y = pos
     grid[x][y]
   end
 
   def []=(pos, value)
     x, y = pos
-    tile = grid[x][y]
-    tile.value = new_value
+    grid[x][y].value = value
   end
 
   def columns
-    rows.transpose!
+    rows.transpose
   end
 
   def render
-    puts "(0..8).to_a.join(" ")"
+    puts "  #{(0..8).to_a.join(" ")}"
     grid.each_with_index do |row, i|
       puts "#{i} #{row.join(" ")}"
     end
@@ -51,17 +51,17 @@ class Board
     grid.size
   end
 
-  alias_method :rows, :size
+  alias_method :rows, :grid
 
   def solved?
     rows.all? { |row| solved_set?(row) } &&
       columns.all? { |col| solved_set?(col) } &&
-      squares.all? { |square| solved_set?(square) }
+      squares.all? { |sq| solved_set?(sq) }
   end
 
   def solved_set?(tiles)
     nums = tiles.map(&:value)
-    nums.sort == (1..9)
+    nums.sort == (1..9).to_a
   end
 
   def square(idx)
@@ -69,9 +69,9 @@ class Board
     x = (idx / 3) * 3
     y = (idx % 3) * 3
 
-    (x..x + 3).each do |j|
-      (y..y + 3).each do |i|
-        tiles << self[i, j]
+    (x...x + 3).each do |j|
+      (y...y + 3).each do |i|
+        tiles << self.grid[i][j]
       end
     end
 
@@ -79,7 +79,11 @@ class Board
   end
 
   def squares
-    (0..8).to_a.each { |i| square(i) }
+    square_array = []
+    (0..8).to_a.each do |i|
+      result << square(i)
+    end
+    square_array
   end
 
 end
